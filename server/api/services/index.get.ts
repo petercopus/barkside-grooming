ef34e458c7252004cdaa5234291a66a3611c73c1
petercopus <petercopus@gmail.com>
2026-03-20 18:34:33 -0500

    Added non-admin endpoint to fetch active services

---
+++ server/api/services/index.get.ts
@@ -0,0 +1,18 @@
+import { getServicePricing, listServices } from '~~/server/services/service.service';
+
+export default defineEventHandler(async (event) => {
+  requireAuth(event);
+
+  // fetch active services
+  const serviceList = await listServices(false);
+
+  // attach pricing
+  const servicesWithPricing = await Promise.all(
+    serviceList.map(async (s) => ({
+      ...s,
+      pricing: await getServicePricing(s.id),
+    })),
+  );
+
+  return { services: servicesWithPricing };
+});
